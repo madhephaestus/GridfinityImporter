0cee481bb6bdc190241fa8bb11a02a6ca310661a
Kevin harrington <mad.hephaestus@gmail.com>
2024-11-02 16:26:30 -0400

    Adding parameters

--- GridfinityImporter.groovy
+++ GridfinityImporter.groovy
@@ -3,6 +3,11 @@
 // https://github.com/kennetek/gridfinity-rebuilt-openscad.git
 // gridfinity-rebuilt-bins.scad
 
+import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
+
+HashMap<String,Double> params=new HashMap<String, Double>();
+
+
 return ScriptingEngine.gitScriptRun(
 	"https://github.com/kennetek/gridfinity-rebuilt-openscad.git",
 	"gridfinity-rebuilt-bins.scad")
