4ddcc5cb70bfc16a9d9e6e13c4d5fc06b2561d3b
Kevin harrington <mad.hephaestus@gmail.com>
2024-11-02 14:15:55 -0400

    Reformat

---
+++ GridfinityImporter.groovy
@@ -0,0 +1,8 @@
+// code here
+
+// https://github.com/kennetek/gridfinity-rebuilt-openscad.git
+// gridfinity-rebuilt-bins.scad
+
+return ScriptingEngine.gitScriptRun(
+	"https://github.com/kennetek/gridfinity-rebuilt-openscad.git",
+	"gridfinity-rebuilt-bins.scad")
